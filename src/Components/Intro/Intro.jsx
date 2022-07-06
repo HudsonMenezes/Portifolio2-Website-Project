02380398f27ad67bfe4ce10cec4cb71c8e5be593
Hudson Menezes <hudson.bm23@gmail.com>
2022-07-06 15:14:12 -0300

    intro section done

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png'
 import Linkedin from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatingDiv from '../FloatingDiv/FloatingDiv'
 
 function Intro() {
   return (
@@ -25,7 +32,28 @@ function Intro() {
 
       </div>
       <div className="i-right">
-        I'm right side
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="" />
+        <img src={glassesimoji} alt="" />
+        <div style={{top: '-4%', left: '68%'}}>
+          <FloatingDiv 
+          image={crown} 
+          txt1='Web'
+          txt2='Developer'/>
+        </div>
+        <div style={{top: '18rem', left: '1.75rem'}}>
+          <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award'/>
+        </div>
+        {/* Blur Divs */}
+        <div className='blur' 
+        style={{background: "rgb(238 210 255"}}></div>
+        <div className='blur' 
+        style={{background: '#c1f5ff',
+        top: '17rem',
+        width: '21rem',
+        height: '11rem',
+        left: '-9rem'}}></div>
       </div>
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,20 +6,20 @@ function Navbar() {
     <div className="n-wrapper">
       <div className="n-left">
         <div className="n-name">Hudson</div>
-        <span>toggle</span>
+        <span>...</span>
       </div>
       <div className="n-right">
         <div className="n-list">
           <ul>
             <li>Home</li>
-            <li>Services</li>
-            <li>Experience</li>
+            <li>Serviços</li>
+            <li>Experiência</li>
             <li>Portfolio</li>
-            <li>Testimonials</li>
+            <li>Feedback</li>
           </ul>
         </div>
         <button className='button n-button'>
-          Entre em Contato
+          Contato
         </button>
       </div>
     </div>
