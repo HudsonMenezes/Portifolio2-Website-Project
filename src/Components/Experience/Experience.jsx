6ba931e6225269dace9cfcb9bee111fab1cd48f7
Hudson Menezes <hudson.bm23@gmail.com>
2022-07-07 10:39:54 -0300

    experience section done

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Experience.css'
+
+function Experience() {
+  return (
+    <div className='experience'>
+      <div className="achievement">
+        <div className="circle">+1</div>
+        <span>ano de</span>
+        <span>experiência</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">+10</div>
+        <span>projetos</span>
+        <span>concluídos</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">+5</div>
+        <span>empresas</span>
+        <span>parceiras</span>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Experience
