5e59ea2a162a72ed07014ff67799db67e999a7b3
Hudson Menezes <hudson.bm23@gmail.com>
2022-07-07 09:16:20 -0300

    services section done

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,6 +3,9 @@ import './Services.css'
 import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
+import Card from '../Card/Card';
+import Resume from './HudsonMenezes-CV.pdf'
+
 
 function Services() {
   return (
@@ -15,15 +18,46 @@ function Services() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit.<br /> 
           Eum minus alias quod dolore ipsam eligendi fugiat sunt quas.
         </span>
+        <a href={Resume} download>
+
         <button className='button s-button'>
           Baixar Currículo
         </button>
+          
+        </a>
         <div className='blur s-blur1' 
         style={{background: '#abf1ff94'}}></div>
       </div>
 
       {/* Right Side */}
-      <div className="cards">I am the Right side</div>
+      <div className="cards">
+        {/* First Card */}
+        <div style={{left: '14rem'}}>
+          <Card 
+            emoji = {HeartEmoji}
+            heading = {'Design'}
+            detail ={"Figma, Sketch, Photoshop, Adobe, Adobe xd"}
+          />
+        </div>
+        {/* Second Card */}
+        <div style={{top: '12rem', left: '-4rem'}}>
+          <Card 
+            emoji = {Glasses}
+            heading = {'Desenvolvimento'}
+            detail ={"Html, Css, JavaScript, React"}
+          />
+        </div>
+        {/* Third Card */}
+        <div style={{top: '19rem', left: '12rem'}}>
+          <Card 
+            emoji = {Humble}
+            heading = {'UI/UX'}
+            detail ={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, magnam!'}
+          /> 
+        </div>
+        <div className='blur s-blur2'
+         ></div>
+      </div>
     </div>
   )
 }
